01991ccd51fb8ba952aa356c6ed08c5a4f2ae5fd
RamiAmasha31 <ramiamasha84@gmail.com>
2024-03-03 19:41:39 +0200

    data is changed to json format and saved as js object

--- models/dishesModel.js
+++ models/dishesModel.js
@@ -1,67 +1,90 @@
 export const dishesData = [
     {
-        title: 'Essential Package',
-        description: 'Perfect for small gatherings. Includes a selection of our signature dishes.',
-        price: 99.58,
-        imgSrc: 'images/catering/image1.jpeg',
+      "title": "Filet Mignon",
+      "description": "A tender and lean cut of beef often grilled or pan-seared and served with various sauces.",
+      "price": 55,
+      "imgSrc": "https://scontent.ftlv1-1.fna.fbcdn.net/v/t39.30808-6/275192130_1014177602506921_4183310487061422317_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=dd5e9f&_nc_ohc=ZfmBUexbcnoAX_-FN6R&_nc_ht=scontent.ftlv1-1.fna&oh=00_AfABbK2Xm9DQYmfVX2bfWCSO6EpZdCU_AaX5SoUksuhuHg&oe=65E56882"
     },
     {
-        title: 'Essential Package',
-        description: 'Perfect for small gatherings. Includes a selection of our signature dishes.',
-        price: 99.58,
-        imgSrc: 'images/catering/image1.jpeg',
+      "title": "Steak au Poivre",
+      "description": "A classic French dish featuring a peppercorn-crusted steak with a creamy pepper sauce.",
+      "price": 40,
+      "imgSrc": "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F288786179%2F904328776353%2F1%2Foriginal.20220520-235204?w=940&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C1280%2C640&s=a59f48b361f44977d79ced7097380a8f"
     },
     {
-        title: 'Essential Package',
-        description: 'Perfect for small gatherings. Includes a selection of our signature dishes.',
-        price: 99.58,
-        imgSrc: 'images/catering/image1.jpeg',
+      "title": "Beef fillet medallions",
+      "description": "Beef fillet medallions are expertly prepared through grilling or pan-searing to perfection.",
+      "price": 50,
+      "imgSrc": "https://hatraklin.co.il/wp-content/uploads/sites/471/2022/04/gall02_0001_Layer-6.jpg"
+    },
+    {
+      "title": "Baby Prime Rib",
+      "description": "Entrecote on the bone",
+      "price": 30,
+      "imgSrc": "https://cms.saharalasvegas.com/wp-content/uploads/2020/04/Restaurant-and-Bar-3-1024x512.jpg"
     }
-    // Add more data objects as needed
-];
+  ];
 
 
 
 
 export const drinksData = [
     {
-        title: 'Essential Package',
-        description: 'Perfect for small gatherings. Includes a selection of our signature dishes.',
-        price: 99.58,
-        imgSrc: 'images/catering/image1.jpeg',
+   
+      "title": "Water",
+      "description": "",
+      "price": 5,
+      "imgSrc": "https://media.istockphoto.com/id/474109210/photo/closeup-of-bottle-of-water-pouring-into-a-glass.jpg?s=1024x1024&w=is&k=20&c=pBaGHkS8rc7MhC8dmNDiUalva-DTtXQ4Wi-c2CyuDik="
     },
     {
-        title: 'Essential Package',
-        description: 'Perfect for small gatherings. Includes a selection of our signature dishes.',
-        price: 99.58,
-        imgSrc: 'images/catering/image1.jpeg',
+  
+      "title": "Orange Juice",
+      "description": "",
+      "price": 7,
+      "imgSrc": "https://t3.ftcdn.net/jpg/00/74/17/32/360_F_74173277_zJV3IktkJX8jfJ6KNbotZmGXgAkw2Yes.jpg"
     },
     {
-        title: 'Essential Package',
-        description: 'Perfect for small gatherings. Includes a selection of our signature dishes.',
-        price: 99.58,
-        imgSrc: 'images/catering/image1.jpeg',
+    
+      "title": "Cola",
+      "description": "",
+      "price": 7,
+      "imgSrc": "https://t3.ftcdn.net/jpg/03/69/56/02/360_F_369560255_ze7zKUVKic1yQKzmXOSym2shcEyGqKPg.jpg"
+    },
+    {
+  
+      "title": "Soda",
+      "description": "",
+      "price": 7,
+      "imgSrc": "https://media.istockphoto.com/id/1181609421/photo/glass-of-carbonated-water-with-ice.jpg?s=612x612&w=0&k=20&c=s265A-SmZcoT4iV3IITrMEbTjISFhmC9NBBh8n14sGg="
     }
-    // Add more data objects as needed
-];
+  ];
 export const alcoholData = [
     {
-        title: 'Essential Package',
-        description: 'Perfect for small gatherings. Includes a selection of our signature dishes.',
-        price: 19.58,
-        imgSrc: 'images/catering/image1.jpeg',
+
+      "title": "INVISIBLE",
+      "description": "A sophisticated cocktail that contains cherry liqueur gin and herbal liqueur with hints of cocoa and coffee.",
+      "price": 15,
+      "imgSrc": "https://images.rest.co.il/Customers/80365907/c57dd7074cfc4d19bae2d95ccc8ad26a.jpg"
     },
     {
-        title: 'Essential Package',
-        description: 'Perfect for small gatherings. Includes a selection of our signature dishes.',
-        price: 19.58,
-        imgSrc: 'images/catering/image1.jpeg',
+
+      "title": "CREAM ON TOP",
+      "description": "A cocktail based on mixed whiskey and tonka nut syrup that comes from South America with unique flavors of vanilla, pistachio and coffee served with red wine whipped cream and spices. A sweet and spicy cocktail with a delicate complexity accompanied by the flavors of sangria.",
+      "price": 20,
+      "imgSrc": "https://images.rest.co.il/Customers/80365907/a319698f950c4149b937e1540daad44a.jpg"
     },
     {
-        title: 'Essential Package',
-        description: 'Perfect for small gatherings. Includes a selection of our signature dishes.',
-        price: 19.58,
-        imgSrc: 'images/catering/image1.jpeg',
+
+      "title": "HEMINGWAY IN LONDON",
+      "description": "A gin-based cocktail accompanied by grapefruit cordial and a special lemon concentration with a salt water compound. Citrus cocktail, sour, slightly herbaceous and salty.",
+      "price": 25,
+      "imgSrc": "https://images.rest.co.il/Customers/80365907/a3734a9690af4eb78de82e7069bd6ee7.jpg"
+    },
+    {
+
+      "title": "COAL SUGAR",
+      "description": "A beet-infused rum-based cocktail combined with Italian liqueur, almond syrup and passionfruit. A sour and citrusy cocktail with sweet carnivorous flavors.",
+      "price": 20,
+      "imgSrc": "https://images.rest.co.il/Customers/80365907/1a7fc921125b452ab692e8d14602f66c.jpg"
     }
-    // Add more data objects as needed
-];
+  ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-
+import { dishesData,drinksData,alcoholData } from '../models/dishesModel.js'
 import aboutUsData from '../models/aboutUsModel.js'
 import reservationsData from '../models/reservationsModel.js'
 import galleryData from '../models/galleryModel.js'
@@ -7,12 +7,12 @@ import privateEventsData from '../models/privateEventsModel.js'
 import heroData from '../models/heroModel.js'
 import {injectHeroSection,injectPrivateEvents,injectCaering,injectGalleryData,injectReservationForm,injectAboutUsData,renderCards} from './injectors.js'
 import initializeScript from './initScript.js'
-import { db_load } from './dataLoad.js'
 
-
-db_load();
 initializeScript();
 injectHeroSection(heroData);
+renderCards(dishesData);
+renderCards(drinksData);
+renderCards(alcoholData);
 injectAboutUsData(aboutUsData);
 injectReservationForm(reservationsData);
 injectGalleryData(galleryData);
